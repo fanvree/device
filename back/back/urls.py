de553827875c108c504273708020ffc2ed970deb
ukyotachibana0 <3298825551@qq.com>
2020-09-10 16:52:11 +0800

    str

--- back/back/urls.py
+++ back/back/urls.py
@@ -24,20 +24,32 @@ from fzr.views import owner_order_list
 from fzr.views import owner_device_order_change
 from django.conf.urls import url
 from szl import views
+from zsw.views import login
+from zsw.views import logout
+from zsw.views import get_user
+from zsw.views import delete_user
+from zsw.views import set_user
+from zsw.views import get_device
+from zsw.views import delete_device
+from zsw.views import edit_device
+from zsw.views import get_shelf_device
+from zsw.views import order_device
+from zsw.views import get_order_history
+from zsw.views import get_self_rented_device
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^sendemail$', send_email),
     url(r'^logon$', logon),
-    url(r'/boss/order/list',views.GetOrderList),
-    url(r'/boss/order/state',views.ChangeOrderState),
-    url(r'/boss/order/delete',views.DeleteOrder),
-    url(r'/boss/offer/list',views.GetOfferList),
-    url(r'/boss/offer/state',views.ChangeOfferState),
-    url(r'/boss/offer/delete',views.DeleteOffer),
-    url(r'/boss/shelf/list',views.GetShelfList),
-    url(r'/boss/shelf/state',views.ChangeShelfState),
-    url(r'/boss/shelf/delete',views.DeleteShelf)
+    url(r'^boss/order/list$', views.GetOrderList),
+    url(r'^/boss/order/state$', views.ChangeOrderState),
+    url(r'^/boss/order/delete$', views.DeleteOrder),
+    url(r'^/boss/offer/list$', views.GetOfferList),
+    url(r'^/boss/offer/state$', views.ChangeOfferState),
+    url(r'^/boss/offer/delete$', views.DeleteOffer),
+    url(r'^/boss/shelf/list$', views.GetShelfList),
+    url(r'^/boss/shelf/state$', views.ChangeShelfState),
+    url(r'^/boss/shelf/delete$', views.DeleteShelf),
     url(r'^/boss/order/list$', views.GetOrderList),
     url(r'^/boss/order/state$', views.ChangeOrderState),
     url(r'^/boss/order/delete$', views.DeleteOrder),
@@ -45,5 +57,17 @@ urlpatterns = [
     url(r'^/owner/device/add$', owner_device_add),
     url(r'^/owner/device/change$', owner_device_change),
     url(r'^/owner/device/order$', owner_order_list),
-    url(r'^/owner/device/order/change$', owner_device_order_change)
+    url(r'^/owner/device/order/change$', owner_device_order_change),
+    url(r'^login$', login),
+    url(r'^logout$', logout),
+    url(r'^boss/user/list$', get_user),
+    url(r'^boss/user/delete$', delete_user),
+    url(r'^boss/user/set$', set_user),
+    url(r'^boss/device/list$', get_device),
+    url(r'^boss/device/delete$', delete_device),
+    url(r'^boss/device/change$', edit_device),
+    url(r'^user/device/list$', get_shelf_device),
+    url(r'^user/device/lend$', order_device),
+    url(r'^user/order/history$', get_order_history),
+    url(r'^user/device/own$', get_self_rented_device)
 ]
