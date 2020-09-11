bd0be490b5a8f15042318b1ea443343225a3cb4c
ukyotachibana0 <3298825551@qq.com>
2020-09-11 11:45:00 +0800

    zsw

--- back/database/admin.py
+++ back/database/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Device, RentingOrder
+from .models import User, Device, RentingOrder, ShelfOrder, ApplyOrder
 
 # Register your models here.
 admin.site.register(User)
